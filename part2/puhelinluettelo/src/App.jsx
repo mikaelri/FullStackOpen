1968ed6da64601e4a6b4cbb3127dd0008ee6d989
mikaelri <mikael.rintala@outlook.com>
2023-12-14 10:18:16 +0200

    return ex. 2.9

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -2,37 +2,51 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-12331244'
-    }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+    { name: 'Mary Marylin', number: '49-24-6423112' }
+  ])
+
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const [filterValue, setFilterValue] = useState('');
+  const [showFiltered, setShowFiltered] = useState(persons);
+
+  const handleFilter = (event) => {
+    const value = event.target.value;
+    setFilterValue(event.target.value);
+    const filtered = persons.filter((person) => person.name.toLowerCase().includes(value.toLowerCase()));
+    setShowFiltered(filtered);
+  };
 
 const addPerson = (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
     if (persons.some(person => person.name === newName)) {
     alert(`${newName} is already added to phonebook`)
-    return }
+    return };
 
-  setPersons([...persons, { name: newName, number: newNumber}])
-  setNewNumber('')
+  setPersons([...persons, { name: newName, number: newNumber}]);
+  setNewNumber('');
   setNewName('');
 }
 
 const handleNameChange = (event) => {
   setNewName(event.target.value)
-  } 
+  };
 
 const handleNumberChange = (event) => {
   setNewNumber(event.target.value)
-}
-
+};
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <>filter shown with <input type="text" value={filterValue} onChange={handleFilter}/></>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
         name: <input value={newName} onChange={handleNameChange} /><br></br>
@@ -42,8 +56,10 @@ const handleNumberChange = (event) => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person, id) => (
-        <p key={id}>{person.name} {person.number}</ p>
+        {showFiltered.map((person, id) => (
+          <p key={id}>
+            {person.name} {person.number}
+            </p>
         ))}
       </div>
     </div>
