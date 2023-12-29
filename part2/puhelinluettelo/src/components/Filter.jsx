af921f001ccbacee569270c8589e11d25224f829
Mikael Rintala <125586308+mikaelri@users.noreply.github.com>
2023-12-29 12:34:36 +0200

    Delete old folder

--- part2/puhelinluettelo/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Filter = ({filterValue, handlefilterChange}) => {
-    return (
-    <>filter shown with <input type="text" value={filterValue} onChange={handlefilterChange}/></>
-    )
-};
-
-export default Filter;
