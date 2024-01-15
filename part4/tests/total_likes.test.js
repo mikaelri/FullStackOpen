322efad8a604cf1759597bedb100ee4c129e0234
mikaelri <mikael.rintala@outlook.com>
2024-01-15 13:55:06 +0200

    return ex.4.5

--- part4/tests/total_likes.test.js
+++ part4/tests/total_likes.test.js
@@ -1,11 +1,11 @@
-const listHelper = require('../utils/list_helper')
+const listHelper  = require('../utils/list_helper')
 const { listWithoutBlogs, listWithOneBlog, listWithManyBlogs } = require('./blogs_test_helper')
 
 describe('total likes', () => {
 
   test('of empty list is zero', () => {
-    const result = listHelper.dummy(listWithoutBlogs)
-    expect(result).toBe(1)
+    const result = listHelper.totalLikes(listWithoutBlogs)
+    expect(result).toBe(0)
   })
 
   test('when list has only one blog equals the likes of that', () => {
@@ -16,5 +16,20 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
     expect(result).toBe(36)
+  })
+})
+
+describe('of most likes returns correctly', () => {
+
+  test('most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(
+      {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12,
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html'
+      }
+    )
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,7 +4,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map(blog => Number(blog.likes)))
+  const targetBlog = blogs.find( blog => Number(blog.likes) === maxLikes)
+
+  return ( {
+    title: targetBlog.title,
+    author: targetBlog.author,
+    likes: targetBlog.likes,
+    url: targetBlog.url
+  })
+
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
