ece3d803d9389ecc17b4263f8a87a596c7c3961a
mikaelri <mikael.rintala@outlook.com>
2024-01-16 12:19:47 +0200

    return ex.4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -37,8 +37,15 @@ test('blogs are returned as json', async () => {
 
 test('correct amount of blogs is found from the list of blogs', async () => {
   const response = await api.get('/api/blogs')
-
   expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('blogs have a field named "id"', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => {
+    expect(blog.id).toBeDefined()
+  })
+
 })
 
 afterAll(async () => {
