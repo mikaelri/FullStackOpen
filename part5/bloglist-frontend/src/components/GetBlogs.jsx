b8238cf070ede159137044309da1a93f8f5427fd
mikaelri <mikael.rintala@outlook.com>
2024-03-01 17:36:07 +0200

    return ex. 5.7

--- part5/bloglist-frontend/src/components/GetBlogs.jsx
+++ part5/bloglist-frontend/src/components/GetBlogs.jsx
@@ -1,16 +1,44 @@
-const Blog = ({ blogs }) => {
-  
+import React, { useState } from "react"
+
+const Blog = ({ blogs}) => {
+  const [visible, setVisible] = useState({})
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 3,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 7,
+  }  
+
+  const toggleVisibility = (id) => {
+    setVisible(prevVisibleBlogs => ({
+      ...prevVisibleBlogs,
+      [id]: !prevVisibleBlogs[id]
+    }))
+  }
+
   return (
     <div>
-      <div>
-        {blogs.map(blog =>
-        <div key={blog.id}> 
-          <div>
-          { blog.title } { blog.author }
+        {blogs.map(blog => {
+        const isVisible = visible[blog.id]
+        return (
+          <div key={blog.id}style={blogStyle}>
+            {blog.title} {blog.author} <button onClick={() => toggleVisibility(blog.id)}>{isVisible ? 'hide' : 'view'}
+            </button>
+
+            {isVisible && (
+              <div>
+                {blog.url}
+                <br />
+               {blog.likes} <button>like</button>
+               <br />
+               { blog.user.name}
+                </div>
+            )}
           </div>
-        </div>
-      )}
-      </div>
+        );
+      })}
     </div>
   )
 }
