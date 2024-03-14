fa6c94ae37f0181407ad4070aa303369597ff9bc
mikaelri <mikael.rintala@outlook.com>
2024-03-14 10:32:10 +0200

    return ex 5.17

---
+++ part5/bloglist-e2e/tests/blog.spec.js
@@ -0,0 +1,25 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'test',
+        password: 'secret'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = await page.getByText('log in to application')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+
+    await page.getByRole('button', { name: 'login' }).click()
+  })
+})
