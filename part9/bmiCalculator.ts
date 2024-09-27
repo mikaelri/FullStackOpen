1b68594d137beb2451b9cc1399145563afc150d1
mikaelri <mikael.rintala@outlook.com>
2024-09-27 16:40:17 +0300

    return ex. 9.1

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,16 @@
+type Result = string
+
+const calculateBmi = (height: number, weight: number): Result => {
+    const bmi = weight / ((height / 100) ** 2)
+    
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+    return "Normal range";
+    } else if (bmi <= 25.0 && bmi < 29.9) {
+        return "Overweight"
+    } else {
+        return "Obese"
+    }
+}
+console.log(calculateBmi(180, 74))
